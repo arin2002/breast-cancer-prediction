7138f31f7749e9fb16aebfdded788b8ef0c1ad3d
Arin Agarwal <arin.agarwal02@gmail.com>
2023-07-06 07:38:44 +0000

    Jul 6, 2023, 1:08 PM

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 import joblib
 
 # Load the pre-trained model
-model = joblib.load('./breast_cancer_model.pkl')
+model = joblib.load('breast_cancer_model.pkl')
 
 # Function to predict breast cancer
 
@@ -45,14 +45,39 @@ def main():
         "Compactness Mean", min_value=0.0, max_value=0.5, value=0.2, step=0.001)
     concavity_mean = st.number_input(
         "Concavity Mean", min_value=0.0, max_value=1.0, value=0.3, step=0.001)
+    concave_points_mean = st.number_input(
+        "Concave Points Mean", min_value=0.0, max_value=0.2, value=0.1, step=0.001)
     symmetry_mean = st.number_input(
         "Symmetry Mean", min_value=0.0, max_value=1.0, value=0.5, step=0.001)
-    fractal_dimension_mean = st.number_input(
-        "Fractal Dimension Mean", min_value=0.0, max_value=0.2, value=0.1, step=0.001)
+    radius_worst = st.number_input(
+        "Radius Worst", min_value=0.0, max_value=40.0, value=20.0, step=0.1)
+    texture_worst = st.number_input(
+        "Texture Worst", min_value=0.0, max_value=60.0, value=30.0, step=0.1)
+    perimeter_worst = st.number_input(
+        "Perimeter Worst", min_value=0.0, max_value=300.0, value=150.0, step=0.1)
+    area_worst = st.number_input(
+        "Area Worst", min_value=0.0, max_value=4000.0, value=1000.0, step=1.0)
+    smoothness_worst = st.number_input(
+        "Smoothness Worst", min_value=0.0, max_value=0.5, value=0.2, step=0.001)
+    compactness_worst = st.number_input(
+        "Compactness Worst", min_value=0.0, max_value=1.0, value=0.5, step=0.001)
+    concavity_worst = st.number_input(
+        "Concavity Worst", min_value=0.0, max_value=1.0, value=0.5, step=0.001)
+    concave_points_worst = st.number_input(
+        "Concave Points Worst", min_value=0.0, max_value=0.5, value=0.2, step=0.001)
+    symmetry_worst = st.number_input(
+        "Symmetry Worst", min_value=0.0, max_value=1.0, value=0.5, step=0.001)
+    fractal_dimension_worst = st.number_input(
+        "Fractal Dimension Worst", min_value=0.0, max_value=1.0, value=0.5, step=0.001)
 
     # Create a feature array from the user input
-    features = [radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_mean,
-                compactness_mean, concavity_mean, symmetry_mean, fractal_dimension_mean]
+    features = [
+        radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_mean,
+        compactness_mean, concavity_mean, concave_points_mean, symmetry_mean,
+        radius_worst, texture_worst, perimeter_worst, area_worst, smoothness_worst,
+        compactness_worst, concavity_worst, concave_points_worst, symmetry_worst,
+        fractal_dimension_worst
+    ]
 
     # Create a button to predict breast cancer
     if st.button("Predict"):
